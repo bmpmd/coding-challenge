e8784d24c2c40285bedc786e6a6689eb117a7e22
leo juarez <sk8leo@live.com>
2022-08-25 10:40:12 -0400

    finished challenge

---
+++ 8-22-22/main.py
@@ -0,0 +1,21 @@
+import statistics as st
+
+def medianOfArr(*arg0):
+    list = []
+    for i in arg0:
+        list+= i
+    print(st.median(list))
+medianOfArr([1,3], [2])
+medianOfArr([1, 2], [3, 4] )
+
+def mergeLists(*arg0):
+    list = []
+    for i in arg0:
+        list.extend(i)
+    list = sorted(list)
+    print(list)
+
+mergeLists([1,4,5],[1,3,4],[2,6])
+mergeLists()
+mergeLists([])
+
